340a2fd110b4cd9ad8a87e2c9daa8957aeff4f61
daniel piva <imdanielpiva@gmail.com>
2017-07-17 22:48:22 +0000

    Remove some unusable stuffs

---
+++ src/components/Form.vue
@@ -0,0 +1,12 @@
+<template>
+  <s-address></s-address>
+</template>
+
+<script>
+import SAddress from './Index.vue'
+
+export default {
+  components: { SAddress }
+}
+</script>
+
---
+++ src/components/Index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="layout-padding docs-input row justify-center">
+    <div style="width: 100%;">
+      <q-field
+        label="Street Address"
+        helper="Your street address"
+        :error="error2"
+        error-label="Some error"
+      >
+        <q-input v-model="text" />        
+      </q-field>
+      <q-field
+        label="Number Location"
+        helper="Your number location"
+        :error="error2"
+        error-label="Some error"
+      >
+        <q-input v-model="text" />        
+      </q-field>
+      <q-field
+        label="Postal Code"
+        helper="Your postal code"
+        :error="error2"
+        error-label="Some error"
+      >
+        <q-input v-model="text" />        
+      </q-field>
+      <q-field
+        label="City"
+        helper="Your city"
+        :error="error2"
+        error-label="Some error"
+      >
+        <q-input v-model="text" />        
+      </q-field>
+      <q-field
+        label="State"
+        helper="Your state"
+        :error="error2"
+        error-label="Some error"
+      >
+        <q-input v-model="text" />        
+      </q-field>
+      <q-field
+        label="Country"
+        helper="Your country"
+        :error="error2"
+        error-label="Some error"
+      >
+        <q-input v-model="text" />        
+      </q-field>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  QInput,
+  QIcon,
+  QField
+} from 'quasar';
+export default {
+  name: 's-address',
+  components: {
+    QInput,
+    QIcon,
+    QField
+  },
+  data () {
+    return {
+      text: '',
+      error: true,
+      error2: false,
+      loading: true
+    }
+  }
+}
+</script>
